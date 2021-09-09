32edd10019d44b29517e66142a8b09c075907fa6
github-mycode <gitgubmycode@gmail.com>
2021-09-09 17:01:40 +0530

    Added

--- Graph.cs
+++ Graph.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace CodingPractice
 {
-    class Graph
+    public class Graph
     {
+        public bool[,] AdjMatrix;
+        public LinkedList<Vertex>[] AdjList;
+        public Vertex[] VertexList;
+        public int MaxCount;
+        public int VertexCount;
+
+        public Graph(int vertexCount, int maxCount)
+        {
+            this.VertexCount = vertexCount;
+            this.MaxCount = maxCount;
+
+            AdjMatrix = new bool[maxCount, maxCount];
+            AdjList = new LinkedList<Vertex>[maxCount];
+            for(int i= 0; i<maxCount; i++)
+            {
+                for (int j = 0; j < maxCount; j++)
+                {
+                    AdjMatrix[i, j] = false;
+                }
+            }
+            VertexList = new Vertex[maxCount];
+        }
     }
 }
